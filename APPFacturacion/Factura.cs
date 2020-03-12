91eee8b56a594f1bad65e4d27314a14fe5ce7826
LuisaZabala <luisazabala99@gmail.com>
2020-03-12 12:00:38 -0500

    Agregar archivos de proyecto.

---
+++ APPFacturacion/Factura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPFacturacion
+{
+    class Factura
+    {
+        public int idFactura;
+        public DateTime fecha;
+
+        public int IdFactura { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ APPFacturacion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPFacturacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int respuestaInicio;
+
+            Console.WriteLine("Elija una opcion");
+            Console.WriteLine("(1) para gestion de clientes, (2) para gestion de productos, (3) para facturacion y (4) para gestion de reportes y (5) para salir");
+            respuestaInicio = int.Parse(Console.ReadLine());
+
+            while (respuestaInicio != 5)
+            {
+
+            }
+        }
+    }
+}
